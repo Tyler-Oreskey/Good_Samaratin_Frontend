01c118226d8a7d5747b0a6591c3492f87283a86f
Tyler <toreskey@gmail.com>
2018-12-12 16:25:12 -0700

    added react navigation

--- components/App/App.js
+++ components/App/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Header from '../Header/Header'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View>
+        <Header />
       </View>
     );
   }
