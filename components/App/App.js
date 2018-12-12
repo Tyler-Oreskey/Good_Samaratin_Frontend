6549fa7d6845a39be792829de51c2496df1281ad
Tyler <toreskey@gmail.com>
2018-12-12 16:13:28 -0700

    added basic react native setup

---
+++ components/App/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
