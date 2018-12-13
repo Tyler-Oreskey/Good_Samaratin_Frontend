1a5d6f2b746734323bd762e0d333f0d9b9050ee3
Tyler <toreskey@gmail.com>
2018-12-13 09:28:04 -0700

    home for timmy tim tim

--- components/App/App.js
+++ components/App/App.js
@@ -6,13 +6,35 @@ export default class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      
+      emergencies: [],
     }
   }
+
+  //make get request to display emergencies on emergencies page
+  async componentDidMount() {
+    const response = await fetch('https://safe-sands-98677.herokuapp.com/emergencies', {
+      method: 'GET',
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        'Accept': 'application/JSON',
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
+    this.setState({
+      ...this.state,
+      emergencies: json
+    })
+  }
+
+
   render() {
     return (
       <View>
         <Navbar />
+        <EmergencyList emergencies={this.state.emergencies}
       </View>
     );
   }
