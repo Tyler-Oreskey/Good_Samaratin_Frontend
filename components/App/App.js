709fecd9d8ff67c73fe64959b78ea38a07106515
Tyler <toreskey@gmail.com>
2018-12-13 08:36:40 -0700

    added login and auth

--- components/App/App.js
+++ components/App/App.js
@@ -3,6 +3,12 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import Navbar from '../Navbar/Navbar'
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <View>
