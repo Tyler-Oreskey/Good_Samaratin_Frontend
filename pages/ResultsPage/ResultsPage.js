065870c7502e6acfe515fa9e12938fc2943f3509
TaylorArmstrong <TArmstrong1327@gmail.com>
2018-12-14 16:02:10 -0700

    styling changes, now onto quiz and results

--- pages/ResultsPage/ResultsPage.js
+++ pages/ResultsPage/ResultsPage.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Dimensions} from 'react-native';
+import React, { Component } from 'react';
+import { Image, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Container, Header, Content, Footer, Button, Left, Right, Body, Title } from 'native-base'
 
@@ -8,11 +8,18 @@ export default class ResultsPage extends Component {
   render() {
       return (
         <Container>
-          <Header>
+          <Header style={styles.header}>
             <Left>
+              <Button style={styles.button}
+                onPress={() => { Actions.questionsPage() }}>
+                <Text style={styles.text}>Back</Text>
+              </Button>
             </Left>
             <Body>
-              <Title></Title>
+              <Image
+                style={styles.logo}
+                source={require('../../Logo/logo.png')}
+              />
             </Body>
             <Right>
             </Right>
@@ -22,6 +29,26 @@ export default class ResultsPage extends Component {
           <Footer>
           </Footer>
         </Container>
-      );
+      )
     }
   }
+
+const styles = {
+  header: {
+    marginTop: 20,
+    height: 120
+  },
+  logo: {
+    width: 70,
+    height: 80,
+  },
+  button: {
+    marginLeft: 10,
+    marginRight: 10,
+    width: 70,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white'
+  }
+}
