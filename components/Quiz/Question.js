a98543aac57ca48b81a8aecb7b01a3065b1dc5ba
TaylorArmstrong <TArmstrong1327@gmail.com>
2018-12-17 01:33:35 -0700

    quiz displaying, with newly structured fake data, but no functionality yet

---
+++ components/Quiz/Question.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Text } from 'react-native'
+import { Container } from 'native-base'
+
+
+class Question extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         incorrectAnswer: false,
+    //         correctAnswer: false,
+    //         showNextQuestionButton: false,
+    //         endQuiz: false,
+    //         currentQuestionIndex: 0,
+    //         buttons: {},
+    //         buttonClasses: {},
+    //         correct: [],
+    //         incorrect: [],
+    //     };
+    // }
+
+    // checkAnswer = (index, correctAnswer) => {
+    //     const { correct, incorrect, currentQuestionIndex } = this.state
+    //     if (index == correctAnswer) {
+    //         if (incorrect.indexOf(currentQuestionIndex) < 0 && correct.indexOf(currentQuestionIndex) < 0) {
+    //             correct.push(currentQuestionIndex)
+    //         }
+
+    //         this.setState({
+    //             correctAnswer: true,
+    //             incorrectAnswer: false,
+    //             showNextQuestionButton: true,
+    //             correct: correct
+    //         })
+
+    //         let disabledAll = {
+    //             0: { disabled: true },
+    //             1: { disabled: true },
+    //             2: { disabled: true },
+    //             3: { disabled: true }
+    //         }
+
+    //         this.setState((prevState) => {
+    //             const buttons = Object.assign(
+    //                 {},
+    //                 prevState.buttons,
+    //                 {
+    //                     ...disabledAll,
+    //                     [index - 1]: {
+    //                         className: (index == correctAnswer) ? "correct" : ""
+    //                     },
+    //                 }
+    //             );
+    //             return { buttons };
+    //         })
+    //     } else {
+    //         if (correct.indexOf(currentQuestionIndex) < 0 && incorrect.indexOf(currentQuestionIndex) < 0) {
+    //             incorrect.push(currentQuestionIndex)
+    //         }
+
+    //         this.setState({
+    //             incorrectAnswer: true,
+    //             correctAnswer: false,
+    //             incorrect: incorrect
+    //         })
+    //         this.setState((prevState) => {
+    //             const buttons = Object.assign(
+    //                 {},
+    //                 prevState.buttons,
+    //                 {
+    //                     [index - 1]: {
+    //                         disabled: !prevState.buttons[index - 1]
+    //                     }
+    //                 }
+    //             );
+    //             return { buttons };
+    //         });
+    //     }
+    // }
+
+    // nextQuestion = (currentQuestionIndex) => {
+    //     const { quiz } = this.props
+
+    //     var initState = {
+    //         incorrectAnswer: false,
+    //         correctAnswer: false,
+    //         showNextQuestionButton: false,
+    //         buttons: {},
+    //     }
+
+    //     if (currentQuestionIndex + 1 == quiz.length) {
+    //         this.setState({
+    //             ...initState,
+    //             endQuiz: true
+    //         })
+    //     } else {
+    //         this.setState({
+    //             ...initState,
+    //             currentQuestionIndex: currentQuestionIndex + 1,
+    //         })
+    //     }
+    // }
+
+    render() {
+        const { quiz } = this.props
+        const { questions } = quiz
+        console.log('Question.js: quiz', quiz)
+        console.log('Question.js: question', questions)
+        // let question = questions[this.state.currentQuestionIndex]
+        return (
+            <Container>
+                {questions.map(question => 
+                    <Container key={question.id}>
+                        <Text key={question.id} question={question.question} style={styles.questions}> {question.question}</Text>
+                        <Container key={question.answers.id} answers={question.answers}>
+                            {question.answers.map(answer =>
+                                <Text key={answer} answer={answer} style={styles.answers}>{answer}</Text>
+                            )}
+                        </Container>
+                    </Container>
+                )}
+            </Container>
+        )
+    }
+}
+
+
+// Question.propTypes = {
+//     questions: PropTypes.array,
+// }
+
+export default Question
+
+
+const styles = {
+    questions: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 14,
+    },
+    answers: {
+        textAlign: 'center',
+        fontSize: 14,
+        marginTop: 10,
+    },
+}
--- pages/QuestionsPage/QuestionsPage.js
+++ pages/QuestionsPage/QuestionsPage.js
@@ -1,11 +1,109 @@
 import React, {Component} from 'react';
 import { Image, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { Container, Header, Footer, Button, Left, Right, Body } from 'native-base'
+import { Container, Header, Footer, Button, Left, Right, Body, Content } from 'native-base'
+import Quiz from '../../components/Quiz/Quiz'
 
 export default class QuestionsPage extends Component {
 
+  constructor(props) {
+    super(props) 
+    this.state = {
+      quiz: [],
+    }
+  }
+
+  async loadQuizzes() {
+    console.log('App: loadQuizzes()')
+    const response = await fetch('https://safe-sands-98677.herokuapp.com/quizzes',
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json',
+        }
+      }
+    )
+
+    console.log('App: loadMessages(): response', response)
+    if (response.status === 200) {
+      const resJSON = await response.json()
+      const quizData = {
+        'quizTitle': 'Good Samaritan General Quiz',
+        'questions': [
+          {
+            'id': '1',
+            'question': 'You find a person who is unresponsive and not breathing, what is the first thing you should do?',
+            'questionType': 'text',
+            'questionCategory': 'basic_quiz',
+            'answers': [
+              'Check for a pulse.',
+              'Start CPR.',
+              'Call 911.',
+              'Check for ID.'
+            ],
+            'correctAnswer': '3'
+          },
+          {
+            'id': '2',
+            'question': 'Your best friend is chowing down on a bratwurst, when he/she grabs his neck and starts hacking, you know they’re is choking, what is the first thing you should do?',
+            'questionType': 'text',
+            'questionCategory': 'basic_quiz',
+            'answers': [
+              'Try to pull the bratwurst out of their throat.',
+              'Perform the Heimlich maneuver.',
+              'Try to burp him/her.',
+              'Give him/her some water.'
+            ],
+            'correctAnswer': '2'
+          },
+          {
+            'id': '3',
+            'question': 'What is the first thing you should do when attempting to help someone else.',
+            'questionType': 'text',
+            'questionCategory': 'basic_quiz',
+            'answers': [
+              'Assess the scene for potential hazards to you. You cannot help someone if you also become a patient/casualty.',
+              'Run to the patient.',
+              'Give the patient a sternum rub to check their responsiveness, even if he/she is making noise.',
+              'Take a photo to send to your friends.'
+            ],
+            'correctAnswer': '1'
+          },
+          {
+            'id': '4',
+            'question': 'When should you help a patient.',
+            'questionType': 'text',
+            'questionCategory': 'basic_quiz',
+            'answers': [
+              'Call 911',
+              'Never',
+              'When you are the most qualified person there and you are willing to help.',
+            ],
+            'correctAnswer': '3'
+          },
+        ]
+      }
+
+      console.log('loaded: ', resJSON)
+      console.log('quizData', quizData)
+      this.setState({
+        quiz: quizData,
+      })
+    } else {
+      console.log('Error:', response)
+      throw new Error('GET request failed')
+    }
+  }
+
+
+  async componentDidMount() {
+    this.loadQuizzes()
+  }
+
   render() {
+    const { quiz } = this.state
+    // console.log('QuestionsPage: quiz', quiz)
       return (
         <Container>
           <Header style={styles.header}>
@@ -31,13 +129,14 @@ export default class QuestionsPage extends Component {
               </Button>
             </Right>
           </Header>
-          <Container>
-            <Button full large rounded primary onPress={() => { Actions.resultsPage(); }}><Text style={styles.text}>Submit</Text></Button>
-          </Container>
+            <Quiz quiz={quiz} />
           <Footer>
+            <Content>
+              <Button full large danger onPress={() => { Actions.homePage() }}><Text>Home Page</Text></Button>
+            </Content>
           </Footer>
-        </Container>
-      );
+          </Container>        
+      )
     }
   }
 
